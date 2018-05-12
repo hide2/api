c5bd74e9bbca33e194913776b6e899df7e186076
hide2 <23771066@qq.com>
2018-05-12 18:13:18 +0800

    x

---
+++ kafka_consumer.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+date_default_timezone_set('PRC');
+use Monolog\Logger;
+use Monolog\Handler\StdoutHandler;
+$logger = new Logger('my_logger');
+$logger->pushHandler(new StdoutHandler());
+
+$config = \Kafka\ConsumerConfig::getInstance();
+$config->setMetadataRefreshIntervalMs(10000);
+$config->setMetadataBrokerList('127.0.0.1:9092');
+$config->setBrokerVersion('1.1.0');
+$config->setGroupId('test');
+$config->setTopics(['test']);
+
+$consumer = new \Kafka\Consumer();
+$consumer->setLogger($logger);
+
+$consumer->start(function($topic, $part, $message) {
+	var_dump($message);
+});
---
+++ kafka_producer.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+date_default_timezone_set('PRC');
+use Monolog\Logger;
+use Monolog\Handler\StdoutHandler;
+$logger = new Logger('my_logger');
+$logger->pushHandler(new StdoutHandler());
+
+$config = \Kafka\ProducerConfig::getInstance();
+$config->setMetadataRefreshIntervalMs(10000);
+$config->setMetadataBrokerList('127.0.0.1:9092');
+$config->setBrokerVersion('1.1.0');
+$config->setRequiredAck(1);
+$config->setIsAsyn(false);
+$config->setProduceInterval(500);
+
+$producer = new \Kafka\Producer();
+$producer->setLogger($logger);
+
+for($i = 0; $i < 100; $i++) {
+    $producer->send([
+        [
+            'topic' => 'test',
+            'value' => 'test....message'.$i,
+            'key' => 'testkey',
+        ],
+    ]);
+}
