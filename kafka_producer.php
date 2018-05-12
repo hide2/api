3a52440f96e6f50e98acca0cc4c159971751825b
hide2 <23771066@qq.com>
2018-05-12 18:16:08 +0800

    x

--- kafka_producer.php
+++ kafka_producer.php
@@ -1,4 +1,6 @@
 <?php
+// bin/zookeeper-server-start.sh config/zookeeper.properties &
+// bin/kafka-server-start.sh config/server.properties &
 require_once __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set('PRC');
 use Monolog\Logger;
