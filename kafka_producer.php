754322f082ff975ecfbdfb85dfb9d591875051bd
hide2 <23771066@qq.com>
2018-05-12 18:37:45 +0800

    x

--- kafka_producer.php
+++ kafka_producer.php
@@ -1,6 +1,10 @@
 <?php
 // bin/zookeeper-server-start.sh config/zookeeper.properties &
 // bin/kafka-server-start.sh config/server.properties &
+// bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
+// bin/kafka-topics.sh --list --zookeeper localhost:2181
+// bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test
+// bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic test --from-beginning
 require_once __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set('PRC');
 use Monolog\Logger;
