edf8a605e72744fac135a7457642d64765954904
hide2 <hideto.bj@gmail.com>
2018-05-18 22:53:31 +0800

    wiki

--- app_ws.php
+++ app_ws.php
@@ -4,25 +4,42 @@ use Workerman\Worker;
 class WSApp extends Worker
 {
 
-	private $map_rpc = array();
+	private $map_path = array();
 
 	public function on($path, callable $callback){
-		$this->map_rpc[$path] = $callback;
+		$this->map_path[$path] = $callback;
 	}
+
+	public function onWebSocketConnect($connection , $http_header)
+    {
+        // params
+		$req = new stdClass();
+		$req->method = $_SERVER['REQUEST_METHOD'];
+		$req->uri = $_SERVER['REQUEST_URI'];
+		$req->path = $req->uri;
+		$pos = stripos($req->path,'?');
+		if ($pos !== false) {
+			$req->path = substr($req->path,0,$pos);
+		}
+		$connection->path = $req->path;
+    }
 
 	public function onClientMessage($connection, $data)
 	{
-		$data = json_decode($data);
-		$cb = $this->map_rpc[$data->method];
+		$cb = $this->map_path[$connection->path];
 		if ($cb) {
-			$data = call_user_func($cb, $data->params);
+			$data = json_decode($data);
+			$data = call_user_func($cb, $data);
 			$connection->send(json_encode($data));
+		} else {
+			$connection->send(json_encode(array('code' => '404')));
 		}
 	}
 
 	public function run()
 	{
 		$this->reusePort = true;
+		$this->onWebSocketConnect = array($this, 'onWebSocketConnect');
 		$this->onMessage = array($this, 'onClientMessage');
 		parent::run();
 	}
--- ws.php
+++ ws.php
@@ -2,23 +2,51 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/app_ws.php';
 
-$wsapp = new WSApp("websocket://0.0.0.0:2000");
+$wsapp = new WSApp("websocket://0.0.0.0:2000/");
 $wsapp->count = 4;
 $wsapp->name = 'ws';
 
-$wsapp->on('api', function($params) {
-	$data = array('name'=>'dad');
-	return $data;
+$wsapp->on('/', function($params) {
+	return array('code' => '200');
+});
+
+$wsapp->on('/api', function($params) {
+	if ($params->action == 'subscribe') {
+		return $params;
+	}
+	return array('code' => '404');
 });
 
 WSApp::runAll();
 
 // JavaScript Client
-// var ws = new WebSocket('ws://127.0.0.1:2000');
+// var ws = new WebSocket('ws://127.0.0.1:2000/');
 // ws.onopen = function(e) {
 // 	console.log("[onopen]"+e);
 // 	var data = {
-// 		method: 'api',
+// 		action: 'aaa',
+// 		params: {
+// 			a: 111,
+// 			b: 'abc'
+// 		}
+// 	}
+// 	ws.send(JSON.stringify(data));
+// }
+// ws.onmessage = function(e) {
+// 	console.log("[onmessage]"+e.data);
+// }
+// ws.onclose = function(e) {
+// 	console.log("[onclose]"+e);
+// }
+// ws.onerror = function(e) {
+// 	console.log("[onerror]"+e);
+// }
+
+// var ws = new WebSocket('ws://127.0.0.1:2000/api');
+// ws.onopen = function(e) {
+// 	console.log("[onopen]"+e);
+// 	var data = {
+// 		action: 'subscribe',
 // 		params: {
 // 			a: 111,
 // 			b: 'abc'
